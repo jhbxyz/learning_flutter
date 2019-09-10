f1e6d42b9ebd852f670d2595228261379d70a3d3
jianghaibo <jhbxyz@163.com>
2019-09-10 09:57:05 +0800

    add images

--- lib/art_route/day2_first_line_app/first_app/first_app.dart
+++ lib/art_route/day2_first_line_app/first_app/first_app.dart
@@ -1,4 +1,54 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 
-void main(){
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyApp',
+      home: MyAppPage(),
+    );
+  }
+}
+
+class MyAppPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppPageState();
+  }
+}
+
+class _MyAppPageState extends State<MyAppPage> {
+  var randomWord = WordPair.random();
+  var wordList = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('haha'),
+      ),
+      body: ListView.separated(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(wordList[index]),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider(
+              height: 1.0,
+            );
+          },
+          itemCount: wordList.length),
+    );
+  }
 }
